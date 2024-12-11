3565412e05d5dd860ac71964308db2b437b4348f
kevinacostaa <kevinmag999@gmail.com>
2024-12-11 16:10:21 +0000

    1-binary_tree_insert_left

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - function
+ * @parent: pointer
+ * @value: value
+ * Return: leftnodo
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *leftnodo, *temp = NULL;
+
+	leftnodo = malloc(sizeof(binary_tree_t));
+
+	if (leftnodo == NULL)
+	{
+		return (NULL);
+	}
+
+	leftnodo->n = value;
+	leftnodo->right = NULL;
+	leftnodo->left = NULL;
+
+	if (parent != NULL)
+	{
+		temp = parent->left;
+		parent->left = leftnodo;
+	}
+	leftnodo->parent = parent;
+
+	if (temp != NULL)
+	{
+		temp->parent = leftnodo;
+		leftnodo->left = temp;
+	}
+
+	return (leftnodo);
+}
