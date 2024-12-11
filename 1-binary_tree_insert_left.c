cda92afda6f375a55b13cb345cac16a27142d5fe
kevinacostaa <kevinmag999@gmail.com>
2024-12-11 16:13:35 +0000

    1-binary_tree_insert_left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,6 +9,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *leftnodo, *temp = NULL;
 
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
 	leftnodo = malloc(sizeof(binary_tree_t));
 
 	if (leftnodo == NULL)
