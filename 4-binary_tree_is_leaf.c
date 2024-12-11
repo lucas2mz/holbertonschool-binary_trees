3cca586f9c60a22d202383f8b4492ece88c332e7
kevinacostaa <kevinmag999@gmail.com>
2024-12-11 16:48:13 +0000

    4-binary_tree_is_leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -11,7 +11,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 		return (0);
 	}
 
-	if(node->right == NULL && node->left == NULL)
+	if (node->right == NULL && node->left == NULL)
 	{
 		return (1);
 	}
