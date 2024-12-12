43ae4b2e36c8b8bb4dba0f1bb9b27299bb51d43b
lucas2mz <lucasandrada2606@gmail.com>
2024-12-12 16:41:02 +0000

    Task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -3,18 +3,40 @@
  *
  *
  * */
-int binary_tree_is_full(const binary_tree_t *tree)
+int is_full(const binary_tree_t *tree)
 {
 	int count = 0;
+
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL && tree->right != NULL)
+	if (!tree->left != !tree->right)
 	{
-		count = 1;
+		count++;
 	}
-	binary_tree_is_full(tree->left);
-	binary_tree_is_full(tree->right);
 
+	count += is_full(tree->right);
+	count += is_full(tree->left);
+
+	return (count);
+}
+
+/**
+ *
+ *
+ * */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int result;
+
+	if (tree == NULL)
+		return (0);
+
+	result = is_full(tree);
+
+	if (result == 0)
+		return (1);
+
+	return (0);
 }
