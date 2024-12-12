ecd4c3ca82c0cd630a9f2f56af5bf69b99f6e712
lucas2mz <lucasandrada2606@gmail.com>
2024-12-12 15:54:27 +0000

    Task 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+/**
+ * count  - Calculates the height of a tree
+ * @tree: Tree to be checked
+ * Return: The height of the tree
+ */
+size_t count(const binary_tree_t *tree)
+{
+	int left_count = 0, right_count = 0;
+
+	if (tree == NULL)
+	{
+		return (-1);
+	}
+
+	left_count = count(tree->left);
+	right_count = count(tree->right);
+
+	if (left_count > right_count)
+	{
+		return (left_count + 1);
+	}
+	else
+		return (right_count + 1);
+}
+/**
+ * binary_tree_balance - Checks if the tree is balanced
+ * @tree: The tree to be checked
+ * Return: The balance of the tree
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int right_count = 0, left_count = 0;
+
+	left_count = count(tree->left);
+	right_count = count(tree->right);
+
+	return (left_count - right_count);
+}
