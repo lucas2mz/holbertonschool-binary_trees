490e4c1cf16ba2648f8f1e0c5a724f6e67c2a984
lucas2mz <lucasandrada2606@gmail.com>
2024-12-14 14:00:27 +0000

    Task 16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_perfect - Checks if the tree is perfect
+ * @tree: Tree to be checked
+ * Return: 1 or 0
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	return (0);
+}
